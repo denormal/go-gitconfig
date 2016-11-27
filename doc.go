e0a2e85f54945ea2c2683e6ba963b89889de9a62
Ian Brayshaw <ian@denormal.co.uk>
2016-11-27 22:43:33 +0000

    Added package documentation.

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package gitconfig provides an interface to git configuration properties
+as returned by "git config --list". gitconfig provides access to local,
+global and system configuration, as well as the effective configuration
+for the given git working copy.
+*/
+package gitconfig
